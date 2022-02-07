c7bf0fcbb6c5f631a6fa1b8b1cf584848f5b1c35
Kyrill Belenkov <bel.kir89@gmail.com>
2022-02-07 15:02:31 +0200

    added onInit func, refactored html

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
 console.log('Starting up the projet gallery');
+
+function onInit() {
+    renderProjs()
+}
+
+function renderProjs() {
+    var strHtml = `
+ <div class="col-md-4 col-sm-6 portfolio-item">
+    <a class="portfolio-link" data-toggle="modal" href="#portfolioModal">
+      <div class="portfolio-hover">
+        <div class="portfolio-hover-content">
+          <i class="fa fa-plus fa-3x"></i>
+        </div>
+      </div>
+      <img class="img-fluid" src="img/portfolio/01-thumbnail.jpg" alt="">
+    </a>
+    <div class="portfolio-caption">
+      <h4>${'TODO Page'}</h4>
+      <p class="text-muted">Illustration</p>
+    </div>
+  </div> 
+  `
+    
+    
+    $('.proj-list').html(strHtml)
+}
